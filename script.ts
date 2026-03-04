7ea5569d1bfed3650551aa33f53531c986063e56
Alauddin Hriday <alauddinhriday@gmail.com>
2026-03-05 01:43:31 +0600

    DB added

--- script.ts
+++ script.ts
@@ -1,40 +1,21 @@
 import { prisma } from "./lib/prisma";
+import { app } from "./server";
+
 
 async function main() {
-  // Create a new user with a post
-//   const user = await prisma.user.create({
-//     data: {
-//       name: "Alice",
-//       email: "alice@prisma.io",
-//       posts: {
-//         create: {
-//           title: "Hello World",
-//           content: "This is my first post!",
-//           published: true,
-//         },
-//       },
-//     },
-//     include: {
-//       posts: true,
-//     },
-//   });
-//   console.log("Created user:", user);
+    try {
+        await prisma.$connect();
+        console.log("DB Connected");
 
-//   // Fetch all users with their posts
-//   const allUsers = await prisma.user.findMany({
-//     include: {
-//       posts: true,
-//     },
-//   });
-//   console.log("All users:", JSON.stringify(allUsers, null, 2));
+        app.listen(3000, () => {
+            console.log(`Server is running at port: 3000`);
+        });
+
+    } catch {
+        console.log("DB Disconnected!");
+        await prisma.$disconnect();
+        process.exit(1);
+    }
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+main();
