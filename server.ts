afc998ce642dbe0a049fd5e76a41ecffcadfa808
Alauddin Hriday <alauddinhriday@gmail.com>
2026-03-07 00:02:03 +0600

    fix some issues

--- server.ts
+++ server.ts
@@ -1,14 +1,20 @@
 import express, { Request, Response } from "express";
 import cors from "cors"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 export const port = process.env.PORT || 5000;
 export const app = express();
 
-app.use(express.json());
 app.use(cors({
     origin: process.env.BETTER_AUTH_URL,
     credentials: true
 }));
+
+app.all("/api/auth/*", toNodeHandler(auth));
+app.use(express.json());
+
+
 
 
 app.get('/', (req: Request, res: Response) => {
